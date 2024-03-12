2c4a80af4b2db48f4481cb1dd1eb288acdd64824
mateufalon <125993891+mateufalon@users.noreply.github.com>
2024-03-12 21:31:00 +0100

    Add files via upload

--- Practica2/code/comun/header.php
+++ Practica2/code/comun/header.php
@@ -3,18 +3,24 @@
 <head>
     <title>Flexam - Plataforma de Tests Universitarios</title>
     <link rel="stylesheet" href="../../styles/styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"> 
 </head>
 <body>
     <header>
-        <h1>FLEXAM</h1>
-        <nav>
-            <ul>
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="tests.php">Tests</a></li>
-                <li><a href="../views/login.php">Login</a></li>
-                <li><a href="../views/sign_up.php">Sign Up</a></li>
-                <!-- Agrega más enlaces según sea necesario -->
-            </ul>
-        </nav>
+        <div class="header-container">
+            <a href="../views/index.php">
+                <img src="../../resources/imagenes/logo_letras.png" alt="FLEXAM logo" class="logo">
+            </a>
+            <div class="separator"></div>
+            <nav>
+                <ul>
+                    <li><a href="../views/miembros.php">Sobre Nosotros</a></li>
+                    <li><a href="../views/info.php">Info</a></li>
+                    <li><a href="../views/menu_tests.php">Tests</a></li>
+                    <li><a href="../views/login.php">Log-in</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </body>
+</html>
